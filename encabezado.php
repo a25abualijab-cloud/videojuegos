9e7e59cdb8dbb0d5df82560d7005573968550864
a25abualijab-cloud <a25abualijab@inspedralbes.cat>
2026-03-13 09:36:31 +0100

    Add files via upload

---
+++ encabezado.php
@@ -0,0 +1,51 @@
+<?php
+/*
+
+  ____          _____               _ _           _       
+ |  _ \        |  __ \             (_) |         | |      
+ | |_) |_   _  | |__) |_ _ _ __ _____| |__  _   _| |_ ___ 
+ |  _ <| | | | |  ___/ _` | '__|_  / | '_ \| | | | __/ _ \
+ | |_) | |_| | | |  | (_| | |   / /| | |_) | |_| | ||  __/
+ |____/ \__, | |_|   \__,_|_|  /___|_|_.__/ \__, |\__\___|
+         __/ |                               __/ |        
+        |___/                               |___/         
+    
+____________________________________
+/ Si necesitas ayuda, contáctame en \
+\ https://parzibyte.me               /
+ ------------------------------------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+Creado por Parzibyte (https://parzibyte.me). Este encabezado debe mantenerse intacto,
+excepto si este es un proyecto de un estudiante.
+*/ ?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD de PHP con MySQLi by parzibyte</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <style>
+        .header-logo {
+            display: flex;
+            align-items: center;
+            gap: 10px;
+            margin-bottom: 20px;
+        }
+        .header-logo img {
+            max-width: 150px;
+            height: auto;
+        }
+    </style>
+</head>
+
+<body>
+    <main class="container-fluid">
+                <div class="header-logo">
+            <img src="https://media.licdn.com/dms/image/v2/C4D0BAQGkZ66n1TziFw/company-logo_200_200/company-logo_200_200/0/1630559246814/institut_ausis_march_logo?e=2147483647&v=beta&t=ZmocxYNTLmdW5JOFY8aRYIjcQmTR3sejRkYJxAaVL7o" alt="Logo Institut">
+        </div>
